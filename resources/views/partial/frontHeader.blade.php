85e56c02605a0a4a754cf952bece7d89596eeb3a
Phong <tranxuanhoat2003@gmail.com>
2024-03-23 21:41:28 +0700

    header

--- resources/views/partial/frontHeader.blade.php
+++ resources/views/partial/frontHeader.blade.php
@@ -1,89 +1,89 @@
 <header>
-    <!-- Navigation bar -->
-    <nav
-            class="relative flex w-full items-center justify-between bg-white py-2 shadow-dark-mild dark:bg-body-dark lg:flex-wrap lg:justify-start lg:py-4"
-            data-twe-navbar-ref>
-            <div class="flex w-full flex-wrap items-center justify-between px-3">
-                <div class="flex items-center">
-                    <!-- Hamburger menu button -->
-                    <button
-                        class="border-0 bg-transparent px-2 text-xl leading-none transition-shadow duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white lg:hidden"
-                        type="button"
-                        data-twe-collapse-init
-                        data-twe-target="#navbarSupportedContentX"
-                        aria-controls="navbarSupportedContentX"
-                        aria-expanded="false"
-                        aria-label="Toggle navigation">
-                        <!-- Hamburger menu icon -->
-                        <span
-                            class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:stroke-black/50 dark:[&>svg]:stroke-neutral-200">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor">
-              <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-            </svg>
-          </span>
-                    </button>
-                </div>
+{{--    <!-- Navigation bar -->--}}
+{{--    <nav--}}
+{{--            class="relative flex w-full items-center justify-between bg-white py-2 shadow-dark-mild dark:bg-body-dark lg:flex-wrap lg:justify-start lg:py-4"--}}
+{{--            data-twe-navbar-ref>--}}
+{{--            <div class="flex w-full flex-wrap items-center justify-between px-3">--}}
+{{--                <div class="flex items-center">--}}
+{{--                    <!-- Hamburger menu button -->--}}
+{{--                    <button--}}
+{{--                        class="border-0 bg-transparent px-2 text-xl leading-none transition-shadow duration-150 ease-in-out hover:text-neutral-700 focus:text-neutral-700 dark:hover:text-white dark:focus:text-white lg:hidden"--}}
+{{--                        type="button"--}}
+{{--                        data-twe-collapse-init--}}
+{{--                        data-twe-target="#navbarSupportedContentX"--}}
+{{--                        aria-controls="navbarSupportedContentX"--}}
+{{--                        aria-expanded="false"--}}
+{{--                        aria-label="Toggle navigation">--}}
+{{--                        <!-- Hamburger menu icon -->--}}
+{{--                        <span--}}
+{{--                            class="[&>svg]:h-7 [&>svg]:w-7 [&>svg]:stroke-black/50 dark:[&>svg]:stroke-neutral-200">--}}
+{{--            <svg--}}
+{{--                xmlns="http://www.w3.org/2000/svg"--}}
+{{--                fill="none"--}}
+{{--                viewBox="0 0 24 24"--}}
+{{--                stroke-width="1.5"--}}
+{{--                stroke="currentColor">--}}
+{{--              <path--}}
+{{--                  stroke-linecap="round"--}}
+{{--                  stroke-linejoin="round"--}}
+{{--                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />--}}
+{{--            </svg>--}}
+{{--          </span>--}}
+{{--                    </button>--}}
+{{--                </div>--}}
 
-                <!-- Navigation links -->
-                <div
-                    class="!visible hidden grow basis-[100%] items-center text-center lg:!flex lg:basis-auto lg:text-left"
-                    id="navbarSupportedContentX"
-                    data-twe-collapse-item>
-                    <ul
-                        class="me-auto flex flex-col lg:flex-row"
-                        data-twe-navbar-nav-ref>
-                        <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                            <a
-                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                                href="#!"
-                                data-twe-nav-link-ref
-                                data-twe-ripple-init
-                                data-twe-ripple-color="light"
-                            >Home</a
-                            >
-                        </li>
-                        <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                            <a
-                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                                href="#!"
-                                data-twe-nav-link-ref
-                                data-twe-ripple-init
-                                data-twe-ripple-color="light"
-                            >Features</a
-                            >
-                        </li>
-                        <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                            <a
-                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                                href="#!"
-                                data-twe-nav-link-ref
-                                data-twe-ripple-init
-                                data-twe-ripple-color="light"
-                            >Pricing</a
-                            >
-                        </li>
-                        <li class="mb-2 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>
-                            <a
-                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"
-                                href="#!"
-                                data-twe-nav-link-ref
-                                data-twe-ripple-init
-                                data-twe-ripple-color="light"
-                            >About</a
-                            >
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+{{--                <!-- Navigation links -->--}}
+{{--                <div--}}
+{{--                    class="!visible hidden grow basis-[100%] items-center text-center lg:!flex lg:basis-auto lg:text-left"--}}
+{{--                    id="navbarSupportedContentX"--}}
+{{--                    data-twe-collapse-item>--}}
+{{--                    <ul--}}
+{{--                        class="me-auto flex flex-col lg:flex-row"--}}
+{{--                        data-twe-navbar-nav-ref>--}}
+{{--                        <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>--}}
+{{--                            <a--}}
+{{--                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"--}}
+{{--                                href="#!"--}}
+{{--                                data-twe-nav-link-ref--}}
+{{--                                data-twe-ripple-init--}}
+{{--                                data-twe-ripple-color="light"--}}
+{{--                            >Home</a--}}
+{{--                            >--}}
+{{--                        </li>--}}
+{{--                        <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>--}}
+{{--                            <a--}}
+{{--                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"--}}
+{{--                                href="#!"--}}
+{{--                                data-twe-nav-link-ref--}}
+{{--                                data-twe-ripple-init--}}
+{{--                                data-twe-ripple-color="light"--}}
+{{--                            >Features</a--}}
+{{--                            >--}}
+{{--                        </li>--}}
+{{--                        <li class="mb-4 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>--}}
+{{--                            <a--}}
+{{--                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"--}}
+{{--                                href="#!"--}}
+{{--                                data-twe-nav-link-ref--}}
+{{--                                data-twe-ripple-init--}}
+{{--                                data-twe-ripple-color="light"--}}
+{{--                            >Pricing</a--}}
+{{--                            >--}}
+{{--                        </li>--}}
+{{--                        <li class="mb-2 lg:mb-0 lg:pe-2" data-twe-nav-item-ref>--}}
+{{--                            <a--}}
+{{--                                class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80 lg:px-2"--}}
+{{--                                href="#!"--}}
+{{--                                data-twe-nav-link-ref--}}
+{{--                                data-twe-ripple-init--}}
+{{--                                data-twe-ripple-color="light"--}}
+{{--                            >About</a--}}
+{{--                            >--}}
+{{--                        </li>--}}
+{{--                    </ul>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </nav>--}}
 
 
 
@@ -107,4 +107,77 @@
             </div>
         </div>
     </div>--}}
+
+    <nav class="border-b-[1px] border-black">
+        <h1 class="py-2 text-center font-semibold text-sm bg-[#79a3dc] text-white">NHẬP MÃ WELCOM - GIẢM 15% CHO ĐƠN TỪ 0đ</h1>
+        <div class="flex px-8 xl:px-20 2xl:px-32 relative">
+            <div class="basis-1/3 md:hidden flex items-center text-2xl text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+
+            <div class="basis-1/3 md:basis-1/4 flex items-center py-3">
+                <a class="flex justify-center" href="#"><img class="w-48" src="{{asset(Config::get('website.logo'))}}" alt=""></a>
+            </div>
+
+            <div class="basis-1/2 py-4 pl-9 xl:pl-0 hidden md:block">
+                <ul
+                    class="flex justify-center h-full">
+                    <li class="group/item flex items-center text-2xl font-semibold hover:font-bold hover:scale-110 px-3 xl:px-8">
+                        <a href="#"
+                        >Home</a>
+                        <div class="group-hover/item:block hidden absolute top-[89px] left-0 w-screen h-fit shadow-sm shadow-gray-700">
+                            <div>
+
+                            </div>
+                            <p class="bg-red-400">Xin chao</p>
+                        </div>
+                    </li>
+                    <li class="flex items-center text-2xl font-semibold hover:font-bold hover:scale-110 px-3 xl:px-8" data-twe-nav-item-ref>
+                        <a
+                            class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+                            href="#!"
+                            data-twe-nav-link-ref
+                            data-twe-ripple-init
+                            data-twe-ripple-color="light"
+                        >Features</a>
+                    </li>
+                    <li class="flex items-center text-2xl font-semibold hover:font-bold hover:scale-110 px-3 xl:px-8" data-twe-nav-item-ref>
+                        <a
+                            class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+                            href="#!"
+                            data-twe-nav-link-ref
+                            data-twe-ripple-init
+                            data-twe-ripple-color="light"
+                        >Pricing</a>
+                    </li>
+                    <li class="flex items-center text-2xl font-semibold hover:font-bold hover:scale-110 px-3 xl:px-8" data-twe-nav-item-ref>
+                        <a
+                            class="block text-black/60 transition duration-200 hover:text-black/80 hover:ease-in-out focus:text-black/80 active:text-black/80 motion-reduce:transition-none dark:text-white/60 dark:hover:text-white/80 dark:focus:text-white/80 dark:active:text-white/80"
+                            href="#!"
+                            data-twe-nav-link-ref
+                            data-twe-ripple-init
+                            data-twe-ripple-color="light"
+                        >About</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="basis-1/3 md:basis-1/4 flex justify-end items-center gap-3 xl:gap-7 text-xl 2xl:text-2xl">
+                <a href="#"><i class="fa-solid fa-user hidden md:block"></i></a>
+                <a href="#"><i class="fa-solid fa-location-dot hidden md:block"></i></a>
+                <a href="#"><i class="fa-solid fa-magnifying-glass pr-2 md:pr-0"></i></a>
+                <a href="#"><i class="fa-solid fa-cart-shopping "></i></a>
+            </div>
+        </div>
+    </nav>
 </header>
+
+<div class="relative h-96 w-96 bg-red-600">
+    <div class="w-64 h-64 mx-10 bg-yellow-300">
+        <div class="absolute w-20 h-20 bg-blue-500 top-0 left-0"></div>
+    </div>
+</div>
+
+
+
+
