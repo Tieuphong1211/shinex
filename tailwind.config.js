2bfea2c73c70ac4e0eb84e69cadbbe5fa4073358
huyhoang2358 <huyhoang.tran6669@gmail.com>
2024-04-08 21:57:39 +0700

    update tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-      "./resources/view/admin/*.blade.php",
-      "./resources/view/admin/**/*.blade.php",
-      "./resources/view/layouts/adminLayout.blade.php",
-      "./resources/view/partial/adminHeader.blade.php",
-      "./resources/view/partial/adminSidebar.blade.php",
+      "./resources/views/admin/*.blade.php",
+      "./resources/views/admin/**/*.blade.php",
+      "./resources/views/layouts/adminLayout.blade.php",
+      "./resources/views/partial/adminHeader.blade.php",
+      "./resources/views/partial/adminSidebar.blade.php",
       "./resources/css/app.css",
       "./resources/js/app.js",
       "./node_modules/tw-elements/js/**/*.js",
